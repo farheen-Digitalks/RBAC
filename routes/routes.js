61337795cefdb81d0d4fe53460620f1c0df2da01
farheen-Digitalks <farheendigitalks@gmail.com>
2026-03-02 15:13:57 +0530

    `Removed unused import of trialRoutes`

--- routes/routes.js
+++ routes/routes.js
@@ -4,7 +4,6 @@ const router = express.Router();
 import userRoutes from "./user.js";
 import permissionRoutes from "./permission.js";
 import roleRoutes from "./role.js";
-import trialRoutes from "./trial.js";
 
 router.use("/users", userRoutes);
 router.use("/permission", permissionRoutes);
