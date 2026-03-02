4d7559a1ba980ad7ccc2f355e0020e055702881b
farheen-Digitalks <farheendigitalks@gmail.com>
2026-03-02 14:23:41 +0530

    `Setting up folders`

---
+++ app.js
@@ -0,0 +1,27 @@
+import express from "express";
+import cors from "cors";
+import { connectDB } from "./config/connecDb.js";
+
+const app = express();
+connectDB();
+
+app.use(
+  cors({
+    origin: "*", // frontend
+    credentials: true, // ⭐ required
+  }),
+);
+app.use(express.json());
+
+import allRoutes from "./routes/index.js";
+app.use("/api", allRoutes);
+
+app.get("/", (req, res) => {
+  res.send("SaaS API running");
+});
+
+app.get("/health", (req, res) => {
+  res.send("OK");
+});
+
+export default app;
